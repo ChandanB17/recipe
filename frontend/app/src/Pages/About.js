a2fa71bc921de698697eff070cdc84afc49266a4
chandan <chandanb200317@gmail.com>
2023-08-15 01:42:31 +0530

    commit

--- frontend/app/src/Pages/About.js
+++ frontend/app/src/Pages/About.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import './About.css'
 const About = () => {
   return (
-    <div>About
-        <h1>about</h1>
-    </div>
-  )
-}
+    <div className="about-section">
+      <h1>About Us Page</h1>
+      <p>Some text about who we are and what we do.</p>
+      <p>Lorem</p>
 
-export default About
+      <h2 style={{ textAlign: 'center' }}>Our Team</h2>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <img src="  " alt="" style={{ width: '100%' }} />
+            <div className="container">
+              <h2>Bala</h2>
+              <p className="title">CEO & Founder</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>bala@example.com</p>
+              <p><button className="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img src="/  /team2.jpg" alt="" style={{ width: '100%' }} />
+            <div className="container">
+              <h2>Abhi</h2>
+              <p className="title">Art Director</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>abhi@example.com</p>
+              <p><button className="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img src="  " alt="" style={{ width: '100%' }} />
+            <div className="container">
+              <h2>Vijay</h2>
+              <p className="title">Designer</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>vijay@example.com</p>
+              <p><button className="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- frontend/app/src/Pages/Home.js
+++ frontend/app/src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <img className="img-tag" src="https://imgs.search.brave.com/SF8I5BYZ8DL--zMFzaUbZzk24qUCx6bkPoSmrKp0Cog/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9i/dWRkaGEtYm93bC1k/aXNoLXdpdGgtdmVn/ZXRhYmxlcy1sZWd1/bWVzLXRvcC12aWV3/XzExNTAtNDI1ODku/anBnP3NpemU9NjI2/JmV4dD1qcGc" alt='loading'></img>
+      {/* <img className="img-tag" src="https://imgs.search.brave.com/SF8I5BYZ8DL--zMFzaUbZzk24qUCx6bkPoSmrKp0Cog/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9i/dWRkaGEtYm93bC1k/aXNoLXdpdGgtdmVn/ZXRhYmxlcy1sZWd1/bWVzLXRvcC12aWV3/XzExNTAtNDI1ODku/anBnP3NpemU9NjI2/JmV4dD1qcGc" alt='loading'></img> */}
       
     </div>
     
