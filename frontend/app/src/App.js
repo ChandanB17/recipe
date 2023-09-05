68b6225c0c7308154dfa65c35a30bfbc108db98b
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-09-05 15:12:00 +0530

    scrolltotop

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import './App.css';
 import Login from './Pages/Login';
@@ -9,22 +8,24 @@ import Contact from './Pages/Contact';
 import Breakfast from './Pages/Breakfast';
 import RecipeListPage from './Pages/RecipeListPage';
 import Footer from './Components/Footer';
+import ScrollToTop from './Components/ScrollToTop';
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path='/Breakfast' element={<Breakfast/>}/>
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/About" element={<About/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
-        <Route path="/AddRecipe" element={<RecipeListPage/>}/>
-
-      </Routes>
-      <Footer/>
-      </BrowserRouter>
+      <Router>
+        <ScrollToTop>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/Breakfast' element={<Breakfast />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/AddRecipe" element={<RecipeListPage />} />
+        </Routes>
+        <Footer />
+        </ScrollToTop>
+      </Router>
 
     </div>
   );
