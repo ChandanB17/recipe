85e8356b2cb9a2f7656e51edd03dc54aa109323e
chandan <chandanb200317@gmail.com>
2023-09-06 01:22:26 +0530

    commit

--- frontend/app/src/Pages/Login.js
+++ frontend/app/src/Pages/Login.js
@@ -20,7 +20,7 @@ function Login() {
             })
             .then(res=>{
                 if(res.data==="exist"){
-                    history("/",{state:{id:email}})
+                      history("/",{state:{id:email}})
                 }
                 else if(res.data==="notexist"){
                     alert("User have not sign up")
